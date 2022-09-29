0e32ea48db12d16b3e8fb2e94c8bf9e783461f6e
“CorinnaLanger” <corinna.langer90@web.de>
2022-09-29 09:29:17 +0200

    scss angelegt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import './Scss/style.scss';
 import Navbar from "./Components/Navbar";
 
 function App() {
-  return <div className="App">Navbar</div>;
+  return (
+    <div>
+<div className="App">Navbar</div>
+<p>Test</p>
+    </div>
+
+  ) ;
 }
 
 export default App;
