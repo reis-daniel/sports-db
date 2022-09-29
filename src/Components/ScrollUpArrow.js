d579b4bbec2bbbf0b1ea13ee05d3842471942b2b
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-09-29 20:46:41 +0200

    Scroll up Components functionality finished. Had to ad some scss too.

--- src/Components/ScrollUpArrow.js
+++ src/Components/ScrollUpArrow.js
@@ -1,7 +1,32 @@
 import React from "react";
+import IconArrow from "../Icons/Icon_Arrow.svg";
+import { useState, useEffect } from "react";
 
 const ScrollUpArrow = () => {
-  return <div className="scroll-up-arrow"></div>;
+  const [showTopBtn, setShowTopBtn] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <div className="scroll-up-arrow">
+      {" "}
+      {showTopBtn && (
+        <img src={IconArrow} alt="Icon arrow up" onClick={goToTop} />
+      )}{" "}
+    </div>
+  );
 };
 
 export default ScrollUpArrow;
