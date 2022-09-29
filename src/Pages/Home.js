9c51fbabae6d0054a828b2651b47a1c01191a2b7
“CorinnaLanger” <corinna.langer90@web.de>
2022-09-29 12:22:28 +0200

    HeaderHome beginning

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 
+import HeaderImG from '../Images/BG_Home.png';
+
 const Leagues = () => {
 
   const[leagues,setleagues] = useState([]);
@@ -13,12 +15,17 @@ const Leagues = () => {
     .then((data) => setleagues(data.leagues));
   }, []);
 return (
+  <div>
+<img className="headerImg" src={HeaderImG} alt="Sports Player"/>
+<p className="headerCaption">Find your <span className="spanLeague">League</span></p>
+ 
   <div className="homeLeagues">
     {leagues.map((l) => (
       <div className="leagueRow">
         <Link to={`/${l.idLeague}`} key={l.idLeague} className="linkLeague">{l.strLeague} <span className="league">{l.strSport}</span></Link> 
         </div>
     ))}
+  </div>
   </div>
 )
 }
