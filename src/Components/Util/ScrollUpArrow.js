6a04b03a8e85fe638419412a75141ab931fb704c
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-10-01 13:25:51 +0200

    filling for scroll up works fine

--- src/Components/Util/ScrollUpArrow.js
+++ src/Components/Util/ScrollUpArrow.js
@@ -1,5 +1,4 @@
 import React from "react";
-import IconArrow from "../../assets/Icons/Icon_Arrow.svg";
 import IconArrowFilled from "../../assets/Icons/Icon_Arrow_filled.png";
 import { useState, useEffect } from "react";
 import "./ScrollUpArrow.scss";
@@ -36,29 +35,29 @@ const ScrollUpArrow = () => {
           className="scroll-up-arrow-desktop"
         >
           <path
-            id="Union"
             d="M15.7069 27.4451L0 54.9396L10.9306 55.227L21.8611 55.5144V76.0155V96.6124H32.4243H42.9874V76.0155V55.5144L53.4587 55.227L63.93 54.9396L48.4068 28.1157C39.8644 13.4584 32.5161 1.00444 32.1487 0.621239C31.7813 0.23804 24.3412 12.3088 15.7069 27.4451Z"
-            fill="transparent"
-            stroke="#E83539"
+            id="arrow-fill"
+            stroke="rgb(232, 53, 57)"
             stroke-width="3"
-          />
+            fill="transparent"
+          ></path>
+
           <mask id="arrow">
             <path
-              id="Union"
               d="M15.7069 27.4451L0 54.9396L10.9306 55.227L21.8611 55.5144V76.0155V96.6124H32.4243H42.9874V76.0155V55.5144L53.4587 55.227L63.93 54.9396L48.4068 28.1157C39.8644 13.4584 32.5161 1.00444 32.1487 0.621239C31.7813 0.23804 24.3412 12.3088 15.7069 27.4451Z"
-              fill="#E83539"
-            />
+              id="arrow-fill"
+              fill="#fff"
+            ></path>
           </mask>
           <g mask="url('#arrow')">
-            <rect id="rect" width="100%" height="100%" fill="#E83539"></rect>
+            <rect
+              id="rect"
+              width="100%"
+              height="100%"
+              fill="rgb(232, 53, 57)"
+            ></rect>
           </g>
         </svg>
-        // <img
-        //   src={IconArrow}
-        //   alt="Icon arrow up"
-        //   onClick={goToTop}
-        //   className="scroll-up-arrow-desktop"
-        // />
       )}{" "}
       {showTopBtn && (
         <img
