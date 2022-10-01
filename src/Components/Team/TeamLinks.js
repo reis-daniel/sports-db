c01b6071c3fce5106028b134bcd587049e85cbc1
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-10-01 14:21:11 +0200

    build main html of Teams Page and started to config TeamLinks

--- src/Components/Team/TeamLinks.js
+++ src/Components/Team/TeamLinks.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+// import axios from "axios";
+// // React
+// import { useEffect, useState } from "react";
+// // Routing
+// import { useLocation, useParams } from "react-router-dom";
+
 const TeamLinks = () => {
-  return <section className="team-links"></section>;
+  return (
+    <section className="team-links">
+      <article className="team-links-details">
+        <a href="#"></a>
+      </article>
+    </section>
+  );
 };
 
 export default TeamLinks;
