21412244995532f3ce49fb591069c3fcd7c14aad
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-10-01 14:37:07 +0200

    added *-styles to styles.scss and worked on TeamLinks component

--- src/Components/Team/TeamLinks.js
+++ src/Components/Team/TeamLinks.js
@@ -1,16 +1,50 @@
 import React from "react";
+import "./TeamLinks.scss";
 
-// import axios from "axios";
-// // React
-// import { useEffect, useState } from "react";
-// // Routing
-// import { useLocation, useParams } from "react-router-dom";
+import axios from "axios";
+// React
+import { useEffect, useState } from "react";
+// Routing
+import { useLocation, useParams } from "react-router-dom";
 
 const TeamLinks = () => {
+  const [team, setTeam] = useState({});
+  const location = useLocation();
+  const params = useParams();
+  const league = location.pathname.split("/")[1].replaceAll("%20", " ");
+
+  const url_teams = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=${league}`;
+
+  useEffect(() => {
+    axios.get(url_teams).then((response) => {
+      // Liste aller Teams
+      let filteredTeam = response.data.teams.filter(
+        (team) => team.strTeam === params.team
+      )[0];
+      setTeam(filteredTeam);
+    });
+  }, []);
+  console.log(team);
   return (
     <section className="team-links">
       <article className="team-links-details">
-        <a href="#"></a>
+        <ul>
+          <li>
+            <a href={team.strWebsite}>Website</a>
+          </li>
+          <li>
+            <a href={team.strFacebook}>Facebook</a>
+          </li>
+          <li>
+            <a href={team.strTwitter}>Twitter</a>
+          </li>
+          <li>
+            <a href={team.strInstagram}>Instagram</a>
+          </li>
+          <li>
+            <a href={team.strYoutube}>Youtube</a>
+          </li>
+        </ul>
       </article>
     </section>
   );
