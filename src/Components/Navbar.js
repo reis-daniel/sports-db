2d944fd75b2858d03251ab45d8b25e590b9d8687
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-10-01 10:20:52 +0200

    Navbar functionality added and styles for Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../Logo/Logo.png";
 
 export default function Navbar() {
-  return <nav>Das wird die Navbar!</nav>;
+  return (
+    <header>
+      <nav>
+        <Link to="/" className="logo">
+          <img src={logo} alt="logo" />
+          <h1>sports.db</h1>
+        </Link>
+        <form action="">
+          <input type="text" name="search" id="inputSearch" />
+        </form>
+      </nav>
+    </header>
+  );
 }
