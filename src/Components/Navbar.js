e9fbb048e20d1f950a4372176d02d598ebfa61cd
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-10-01 10:23:42 +0200

    emoved header tag from navbar and changed logo route

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../Logo/Logo.png";
+import logo from "../assets/Logo/Logo.png";
 
 export default function Navbar() {
   return (
-    <header>
-      <nav>
-        <Link to="/" className="logo">
-          <img src={logo} alt="logo" />
-          <h1>sports.db</h1>
-        </Link>
-        <form action="">
-          <input type="text" name="search" id="inputSearch" />
-        </form>
-      </nav>
-    </header>
+    <nav>
+      <Link to="/" className="logo">
+        <img src={logo} alt="logo" />
+        <h1>sports.db</h1>
+      </Link>
+      <form action="">
+        <input type="text" name="search" id="inputSearch" />
+      </form>
+    </nav>
   );
 }
