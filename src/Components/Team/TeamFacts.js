f09ad678a03f55903454b006589bc09ae7b1be8b
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-10-05 11:02:38 +0200

    if else to check if objects are given

--- src/Components/Team/TeamFacts.js
+++ src/Components/Team/TeamFacts.js
@@ -29,6 +29,7 @@ export default function TeamFacts() {
   return (
     <section className="team-facts">
       <article className="team-fc">
+        {/* CHECK FOR THUMB AND TAKE FALLBACK IF IMG IS NULL */}
         {team.strStadiumThumb === null ? (
           <div className="imgLeague">
             <h2 className="linkLeague">{team.strTeam}</h2>
@@ -44,18 +45,35 @@ export default function TeamFacts() {
           </div>
         )}
         <div className="country">
-          <div className="countryLeague">
-            <h3>{team.strCountry}</h3>
-            <p>Country</p>
-          </div>
-          <div className="countryLeague">
-            <h3>{team.strStadiumLocation}</h3>
-            <p>Location</p>
-          </div>
-          <div className="countryLeague">
-            <h3>{team.intFormedYear}</h3>
-            <p>Established</p>
-          </div>
+          {/* CHECK FOR COUNTRY AND AVOID DIV IF STRCOUNTRY IS NULL */}
+          {team.strCountry === "" ? (
+            ""
+          ) : (
+            <div className="countryLeague">
+              <h3>{team.strCountry}</h3>
+              <p>Country</p>
+            </div>
+          )}
+          {/* CHECK FOR LOCATION AND AVOID DIV IF STRSTADIUMLOCATION IS NULL */}
+          {team.strStadiumLocation === "" ? (
+            ""
+          ) : (
+            <div className="countryLeague">
+              <h3>{team.strStadiumLocation}</h3>
+              <p>Location</p>
+            </div>
+          )}
+          {/* CHECK FOR ESTABLISHED YEAR AND AVOID DIV IF INTFORMEDYEAR IS NULL */}
+          {team.intFormedYear === "" ||
+          team.intFormedYear === 0 ||
+          team.intFormedYear === "0" ? (
+            ""
+          ) : (
+            <div className="countryLeague">
+              <h3>{team.intFormedYear}</h3>
+              <p>Established</p>
+            </div>
+          )}
           <div className="countryLeague">
             <h3>{team.strSport}</h3>
             <p>Sport</p>
