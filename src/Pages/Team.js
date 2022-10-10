971b6786c0b6ea377e589f1c8747f196e700aded
“CorinnaLanger” <corinna.langer90@web.de>
2022-10-10 21:27:45 +0200

    Loading at Team.js

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -1,5 +1,5 @@
 // REACT
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 // COMPONENTS
 import TeamFacts from "../Components/Team/TeamFacts";
 import TeamDescription from "../Components/Team/TeamDescription";
@@ -7,9 +7,15 @@ import TeamStadium from "../Components/Team/TeamStadium";
 import TeamLinks from "../Components/Team/TeamLinks";
 
 export default function Team() {
+
+  const [isLoading, setLoading] = useState(true);
   useEffect(() => {
     window.scrollTo(0, 0);
+    setLoading(false);
   }, []);
+  if (isLoading) {
+    return <div className="TeamsList_onload">Loading...</div>;
+  }
   return (
     <>
       <TeamFacts />
