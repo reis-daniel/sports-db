ac775622a48f51f17fcadf430fd1b3545c128728
Daniel Wurring <daniel.wur@googlemail.com>
2022-10-13 17:24:26 +0200

    no sport when refreshing the page fixed.

--- src/Components/Team/TeamsList.js
+++ src/Components/Team/TeamsList.js
@@ -4,16 +4,19 @@ import "./TeamsList.scss";
 import { useEffect, useState } from "react";
 // PACKAGES
 import { Link, useParams } from "react-router-dom";
-// STORES
+// ZUSTAND STORES
 import useLeaguesStore from "../../Stores/useLeaguesStore";
 import useTeamsStore from "../../Stores/useTeamsStore";
 
 //Fetch to show the teams
 export default function TeamsList() {
-  const { sportOfLeague, setSportOfLeague } = useLeaguesStore((state) => ({
-    sportOfLeague: state.sportOfLeague,
-    setSportOfLeague: state.setSportOfLeague,
-  }));
+  const { leagues, fetchLeagues, sportOfLeague, setSportOfLeague } =
+    useLeaguesStore((state) => ({
+      leagues: state.leagues,
+      fetchLeagues: state.fetchLeagues,
+      sportOfLeague: state.sportOfLeague,
+      setSportOfLeague: state.setSportOfLeague,
+    }));
   const { teams, setTeam, fetchTeams } = useTeamsStore((state) => ({
     teams: state.teams,
     fetchTeams: state.fetchTeams,
@@ -23,9 +26,12 @@ export default function TeamsList() {
   // State again to display the image at the same time as the other content, and when it loads a Loading should be displayed
   const [isLoading, setLoading] = useState(true);
   const params = useParams();
+  const url_leagues =
+    "https://www.thesportsdb.com/api/v1/json/2/all_leagues.php";
 
   useEffect(() => {
     const getTeamsData = async () => {
+      if (leagues.length === 0) await fetchLeagues(url_leagues);
       setSportOfLeague(params.liga);
       await fetchTeams(params.liga);
       setLoading(false);
@@ -40,6 +46,7 @@ export default function TeamsList() {
   return (
     <section>
       <article className="TeamsList_firstArticle">
+        {/* IMAGE NEEDS TO LOAD! */}
         <img
           className="TeamsList_img"
           src={`https://source.unsplash.com/1600x900/?${sportOfLeague}`}
