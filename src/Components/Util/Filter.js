fe67a81d217b9c4ab7a58d48fc1ae63615e6c80e
Daniel Wurring <daniel.wur@googlemail.com>
2022-10-13 12:23:29 +0200

    filterdropdown-container styling was missing

--- src/Components/Util/Filter.js
+++ src/Components/Util/Filter.js
@@ -1,24 +1,8 @@
 import React from "react";
 import Filterdropdown from "./Filterdropdown";
 import "./Filter.scss";
-// Zustand
-import useFilterStore from "../../Stores/useFilterStore";
 
 const Filter = () => {
-  const {
-    filteredCountries,
-    filteredSports,
-    removeFilteredCountries,
-    removeFilteredSport,
-    removeFilteredLeagues,
-  } = useFilterStore((state) => ({
-    filteredCountries: state.filteredCountries,
-    filteredSports: state.filteredSports,
-    removeFilteredCountries: state.removeFilteredCountries,
-    removeFilteredSport: state.removeFilteredSport,
-    removeFilteredLeagues: state.removeFilteredLeagues,
-  }));
-
   return (
     <div className="filterbar">
       <Filterdropdown className="filterdropdown" />
